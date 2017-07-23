4c072e7b557ec6e2d499ea0e1de18ffb97087177
Nurseiit <nurseyit.abdimomyn@gmail.com>
2017-07-23 12:04:28 +0600

    modified static pages

--- client/views/index/other.js
+++ client/views/index/other.js
@@ -1,3 +1,19 @@
 Template.contacts.onCreated (function () {
 	$('html,body').animate({scrollTop: 440}, 500);	
 });
+
+Template.about.onCreated (function () {
+	$('html,body').animate({scrollTop: 440}, 500);	
+});
+
+Template.discounts.onCreated (function () {
+	$('html,body').animate({scrollTop: 440}, 500);	
+});
+
+// Template.payment.onCreated (function () {
+// 	$('html,body').animate({scrollTop: 440}, 500);	
+// });
+
+// Template.feedback.onCreated (function () {
+// 	$('html,body').animate({scrollTop: 440}, 500);	
+// });
