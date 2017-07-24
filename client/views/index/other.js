db4df42e22077eded6464df9ab6b50d9e8b97b22
nurseiit <nurseyit.abdimomyn@gmail.com>
2017-07-24 14:18:12 +0600

    updated static pages

--- client/views/index/other.js
+++ client/views/index/other.js
@@ -1,19 +1,23 @@
+Template.cart.onCreated (function () {
+    $('html,body').animate({scrollTop: 440}, 500);    
+}); 
+
 Template.contacts.onCreated (function () {
-	$('html,body').animate({scrollTop: 440}, 500);	
+    $('html,body').animate({scrollTop: 440}, 500);	
 });
 
 Template.about.onCreated (function () {
-	$('html,body').animate({scrollTop: 440}, 500);	
+    $('html,body').animate({scrollTop: 440}, 500);	
 });
 
 Template.discounts.onCreated (function () {
-	$('html,body').animate({scrollTop: 440}, 500);	
+    $('html,body').animate({scrollTop: 440}, 500);	
 });
 
-// Template.payment.onCreated (function () {
-// 	$('html,body').animate({scrollTop: 440}, 500);	
-// });
+Template.payment.onCreated (function () {
+    $('html,body').animate({scrollTop: 440}, 500);	
+});
 
-// Template.feedback.onCreated (function () {
-// 	$('html,body').animate({scrollTop: 440}, 500);	
-// });
+Template.feedback.onCreated (function () {
+    $('html,body').animate({scrollTop: 440}, 500);	
+});
